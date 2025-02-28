ac5ca59a1b4b45f1a392f08c034c5f14c2eb27e6
Hana <hanabia2014@gmail.com>
2025-02-28 00:01:32 -0600

    first-commmit

---
+++ amazon-clone/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1> Amazon Clone</h1>
+      </div>
+    </>
+  );
+}
+
+export default App
