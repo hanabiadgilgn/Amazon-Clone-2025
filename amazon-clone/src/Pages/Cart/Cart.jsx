007211d22464643cd0b747dbaa69a7148a94b529
Hana <hanabia2014@gmail.com>
2025-03-05 22:31:26 -0600

    Cart functionality part two

--- amazon-clone/src/Pages/Cart/Cart.jsx
+++ amazon-clone/src/Pages/Cart/Cart.jsx
@@ -10,7 +10,7 @@ import { Link } from "react-router-dom";
 function Cart() {
   const[{basket, user}, dispatch]=useContext(DataContext);
   const total = basket.reduce((amount, item)=>{
-    return item.price + amount
+    return item.price * item.amount + amount
   },0)
 
   return (
